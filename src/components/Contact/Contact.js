cb0c975e02eb970ae2b5152353e8d3d3ff89fed0
Karan Kumar Gupta <Karangupta9615@gmail.com>
2022-03-19 13:21:42 +0530

    changed button

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,36 +4,36 @@ function Contact() {
   return (
     <div className="flex flex-col my-5">
       <div className="flex my-2 w-80 h-20 bg-red-500 ml-5 text-white items-center justify-around rounded-xl px-5">
-        <img src="" alt="" className="w-10 h-10" />
+        <img src="https://cdn-icons-png.flaticon.com/128/535/535188.png" alt="" className="w-10 h-10" />
         <div className="flex flex-col items-center">
-          <p className="text-2xl font-bold py-2">My Address</p>
+          <p className="text-2xl font-bold">My Address</p>
           <p className="text-sm py-1">Dakshinpuri, New Delhi-62</p>
         </div>
       </div>
       <div className="flex my-2 w-80 h-20 bg-red-500 ml-5 text-white items-center justify-around rounded-xl px-5">
-        <img src="" alt="" className="w-10 h-10" />
+        <img src="https://cdn-icons-png.flaticon.com/128/1077/1077114.png" alt="" className="w-10 h-10" />
         <div className="flex flex-col items-center">
-          <p className="text-2xl font-bold py-2">Social Profiles</p>
-          <p className="text-sm py-1 flex justify-evenly w-full bg-black">
-            <img src="" alt="" className="w-2 h-2" />
-            <img src="" alt="" className="w-2 h-2" />
-            <img src="" alt="" className="w-2 h-2" />
-            <img src="" alt="" className="w-2 h-2" />
+          <p className="text-2xl font-bold py-1">Social Profiles</p>
+          <p className="text-sm flex justify-evenly w-full">
+            <img src="https://cdn-icons.flaticon.com/png/128/2168/premium/2168281.png?token=exp=1647676344~hmac=b6e1ef547045ddfb39e37bc0829b5764" alt="" className="w-5 h-5" />
+            <img src="https://cdn-icons-png.flaticon.com/128/1384/1384017.png" alt="" className="w-5 h-5" />
+            <img src="https://cdn-icons-png.flaticon.com/128/1384/1384014.png" alt="" className="w-5 h-5" />
+            <img src="https://cdn-icons-png.flaticon.com/128/6244/6244710.png" alt="" className="w-5 h-5" />
           </p>
         </div>
       </div>
       <div className="flex my-2 w-80 h-20 bg-red-500 ml-5 text-white items-center justify-around rounded-xl px-5">
-        <img src="" alt="" className="w-10 h-10" />
+        <img src="https://cdn-icons-png.flaticon.com/128/60/60543.png" alt="" className="w-10 h-10" />
         <div className="flex flex-col items-center">
-          <p className="text-2xl font-bold py-2">Email me</p>
-          <p className="text-sm py-1">Karangupta9615@gmail.com</p>
+          <p className="text-2xl font-bold">Email me</p>
+          <p className="text-sm text-gray-300">Karangupta9615@gmail.com</p>
         </div>
       </div>
       <div className="flex my-2 w-80 h-20 bg-red-500 ml-5 text-white items-center justify-around rounded-xl px-5">
-        <img src="" alt="" className="w-10 h-10" />
+        <img src="https://cdn-icons.flaticon.com/png/128/4212/premium/4212312.png?token=exp=1647676726~hmac=58ae6de9e8a14892029973ffaab3b92e" alt="" className="w-10 h-10" />
         <div className="flex flex-col items-center">
-          <p className="text-2xl font-bold py-2">My resume</p>
-          <button className="text-sm py-1 bg-blue-600 hover:bg-blue-700 px-5 mb-2 shadow-2xl transition duration-200">
+          <p className="text-2xl font-bold">My resume</p>
+          <button className="text-sm my-1 py-1 border border-blue-800 hover:bg-blue-700 px-5 mb-2 shadow-2xl transition duration-200 rounded-md">
             Download
           </button>
         </div>
