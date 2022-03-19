687ddae2aa013f8f212daf5b2d23ab3a7d02fd6f
Karan Kumar Gupta <Karangupta9615@gmail.com>
2022-03-19 15:16:10 +0530

    certification done

--- src/components/Certifications/Certification.js
+++ src/components/Certifications/Certification.js
@@ -2,29 +2,28 @@ import React from 'react'
 
 const ProfileCard = ({name,taglin,img}) => {
   return (
-    <div className='flex flex-col items-center mx-auto bg-gradient-to-r from-gray-400 to-gray-500 w-64 justify-center py-5 my-2'>
+    <div className='flex flex-col items-center mx-auto bg-gradient-to-r from-red-400 to-red-700 w-64 justify-center py-5 my-2'>
         <img src={img} alt='' className='h-10 w-10' />
-        <h1 className='mt-2 text-white font-bold text-xl'>{name}</h1>
+        <h1 className='mt-2 text-white font-bold text-xl px-3'>{name}</h1>
         <p className='text-white text-sm'>{taglin}</p>
     </div>
   )
+
+  
 }
 
 function Certification() {
   return (
     <div>
       <div>
-      <h1 className="text-2xl uppercase font-bold mt-5 flex py-2 border border-b-red-600 justify-center ">
+      <h1 className="text-2xl mx-2 uppercase font-bold mt-5 flex py-2 border border-b-red-600 justify-center ">
         Certifications
       </h1>
-      <p className="text-left text-2xl pl-2 font-bold text-red-600 mt-5">
-        Certifications
-      </p>
       <div className='my-10'>
-        <ProfileCard name="GeekforGeeks" taglin="this is profile card is temporary" img="" />
-        <ProfileCard name="GeekforGeeks" taglin="this is profile card is temporary" img="" />
-        <ProfileCard name="GeekforGeeks" taglin="this is profile card is temporary" img="" />
-        <ProfileCard name="GeekforGeeks" taglin="this is profile card is temporary" img="" />
+        <ProfileCard name="HTML, CSS & JS" taglin="" img="https://cdn-icons-png.flaticon.com/128/1005/1005141.png" />
+        <ProfileCard name="Introduction to Machine Learning" taglin="" img="https://cdn-icons.flaticon.com/png/128/2021/premium/2021717.png?token=exp=1647683638~hmac=4229c9eef4fdb10834ea67c50db3e0d8" />
+        <ProfileCard name="Foundation data everywhere" taglin="" img="https://cdn-icons-png.flaticon.com/128/1449/1449312.png" />
+        <ProfileCard name="Programming for everybody" taglin="" img="https://cdn-icons.flaticon.com/png/128/2721/premium/2721620.png?token=exp=1647683842~hmac=7ba2e9027c82c8161b326070aa0e6961" />
       </div>
     </div>
     </div>
