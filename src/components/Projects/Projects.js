d581ac1e9c95575db6496a06b8454c24203c7c4e
Karan Kumar Gupta <Karangupta9615@gmail.com>
2022-03-19 11:06:11 +0530

    temp commit

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ProfileCard = ({name,taglin,img}) => {
   return (
-    <div className='flex flex-col items-center mx-auto bg-gradient-to-r from-gray-400 to-gray-500 w-64 justify-center py-5 my-2'>
+    <div className='flex flex-col items-center mx-auto bg-gradient-to-r from-red-700 via-red-400 to-red-500 w-64 justify-center py-5 my-2'>
         <img src={img} alt='' className='h-10 w-10' />
         <h1 className='mt-2 text-white font-bold text-xl'>{name}</h1>
         <p className='text-white text-sm'>{taglin}</p>
@@ -12,15 +12,12 @@ const ProfileCard = ({name,taglin,img}) => {
 
 function Projects() {
   return (
-    <div>
-      <h1 className="text-2xl uppercase font-bold mt-5 flex py-2 border border-b-red-600 justify-center ">
+    <div className='mx-5'>
+      <h1 style={{fontFamily:"League Spartan,sans-serif"}} className="text-2xl uppercase font-bold mt-5 flex py-2 border border-b-red-600 justify-center ">
         Projects
       </h1>
-      <p className="text-left text-2xl pl-2 font-bold text-red-600 mt-5">
-        Projects
-      </p>
       <div className='my-10'>
-        <ProfileCard name="GeekforGeeks" taglin="this is profile card is temporary" img="" />
+        <ProfileCard name="Virtual Restaurant" taglin="Realistic restaurant website" img="https://cdn-icons-png.flaticon.com/512/45/45332.png" />
         <ProfileCard name="GeekforGeeks" taglin="this is profile card is temporary" img="" />
         <ProfileCard name="GeekforGeeks" taglin="this is profile card is temporary" img="" />
         <ProfileCard name="GeekforGeeks" taglin="this is profile card is temporary" img="" />
