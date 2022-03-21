2f54c0c3ea7d2cbb5f198e87af5d23e6f80b7451
Karan Kumar Gupta <Karangupta9615@gmail.com>
2022-03-21 23:19:46 +0530

    changed nothing

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ProfileCard = ({name,taglin,img}) => {
   return (
-    <div style={{fontFamily:"Manrope,sans-serif"}} className='hover:scale-105 flex flex-col items-center mx-auto bg-gradient-to-r from-red-700 via-red-400 to-red-500 w-64 justify-center py-5 my-2'>
+    <div style={{fontFamily:"Manrope,sans-serif"}} className='rounded-2xl transition duration-300 hover:scale-105 flex flex-col items-center mx-auto bg-gradient-to-r from-red-700 via-red-400 to-red-500 w-64 justify-center py-5 my-2'>
         <img src={img} alt='' className='h-10 w-10' />
         <h1 className='mt-2 text-white font-bold text-xl'>{name}</h1>
         <p className='text-white text-sm'>{taglin}</p>
@@ -16,7 +16,7 @@ function Projects() {
       <h1 style={{fontFamily:"League Spartan,sans-serif"}} className="text-2xl uppercase font-bold mt-5 flex py-2 border border-b-red-600 justify-center ">
         Projects
       </h1>
-      <div className='my-10'>
+      <div className='my-10 grid grid-cols-1 gap-x-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
         <ProfileCard name="Virtual Restaurant" taglin="Realistic restaurant website" img="https://cdn-icons-png.flaticon.com/512/45/45332.png" />
         <ProfileCard name="Facebook Signup clone" taglin="this is the front-end of the facebook Signup/Sigin" img="https://cdn-icons-png.flaticon.com/512/1828/1828503.png" />
         <ProfileCard name="Single page website" taglin="" img="https://cdn-icons-png.flaticon.com/512/1231/1231223.png" />
